ebd7c07ca60a6c1d595e071600a1096755ad55e1
aina2104 <160872725+aina2104@users.noreply.github.com>
2025-04-19 19:07:29 +1000

    Check duplicate names
    Send messages successfully, 1 uncreated thread for server to read from stdin. No queueing.

--- chatserver.py
+++ chatserver.py
@@ -1,19 +1,19 @@
-from sys import argv, stderr, stdout, exit
+from sys import argv, stderr, stdout, stdin, exit
 from socket import *
-from threading import Event, Lock, Thread, current_thread
+from threading import Event, Thread, current_thread
 from time import sleep
 
 create_all_ports = Event()
 remaining_ports = -1
-counter = 0
-counter_lock = Lock()
 BUFSIZE = 1024
 afk_time = 100
 config_filename = None
 channels = []
-channel_name = []
+channel_names = []
 channel_port = []
 channel_capacity = []
+channel_users = {}  # {channel_names: [user_1, user_2]}
+client_address_users = {}
 
 
 def invalid_command_line():
@@ -56,7 +56,7 @@ def check_channel_name(name):
 
 
 def check_file_format(line):
-    global channel_name
+    global channel_names
     global channel_port
     global channel_capacity
     if len(line) != 4 or line[0] != "channel":
@@ -66,9 +66,9 @@ def check_file_format(line):
         invalid_file()
     port, capacity = int(line[2]), int(line[3])
     if (port < 1024 or port > 65535 or capacity < 1 or capacity > 8 or
-            line[1] in channel_name or port in channel_port):
+            line[1] in channel_names or port in channel_port):
         invalid_file()
-    channel_name.append(line[1])
+    channel_names.append(line[1])
     channel_port.append(port)
     channel_capacity.append(capacity)
 
@@ -86,7 +86,7 @@ def check_valid_file():
 def start_server(port_num, index):
     global channels
     global remaining_ports
-    name = channel_name[index]
+    name = channel_names[index]
     capacity = channel_capacity[index]
 
     listening_socket = socket(AF_INET, SOCK_STREAM)
@@ -102,38 +102,49 @@ def start_server(port_num, index):
           f" of {capacity}.\n", flush=True)
     remaining_ports -= 1  # tell program 1 port has been created
     create_all_ports.wait()  # wait until all ports are created to continue
-    process_connections(listening_socket)
+    process_connections(listening_socket, index)  # generate threads per client
 
 
-def handle_client(client_socket, client_address):
-    global counter
-    host,_ = getnameinfo(client_address, 0)
-    print(f"Connection from {client_address} ({host}) has been established.")
+def duplicate_usernames(username, channel_name):
+    if username in channel_users[channel_name]:
+        return True
+    return False
+
+
+def handle_client_messages(message, index, client_address):
+    global channel_users
+    global client_address_users
+    if message[:7] == "$User: ":
+        client_username = message[7:]
+        channel_name = channel_names[index]
+        client_address_users[client_address] = [client_username]
+        if duplicate_usernames(client_username, channel_name):
+            return f"$UserError: {channel_name}"
+        channel_users[channel_name].append(client_username)
+        print(f"[Server Message] {client_username} has joined the channel \"{channel_name}\".\n", file=stdout)
+        return "$Username successful"
+    return "$Pass"
+
+
+def handle_client(client_socket, client_address, index):
     with client_socket:
-        # Send a message to the client
-        message = f"Welcome to the service.\n"
-        client_socket.sendall(message.encode())
-
-        while data := client_socket.recv(BUFSIZE):
-            # Locking the shared resource (counter)
-            with counter_lock:
-                print(f"Thread {current_thread().name} got the lock.")
-                counter += 1
-                print(f"Counter value updated to: {counter}")
-                # Simulate doing some work
-                sleep(2)
-                data = data.decode().upper() 
-                client_socket.sendall(data.encode())
-        # error or EOF - client disconnected
-
-    print(f"Connection from {client_address} closed.")
+        try:
+            while data := client_socket.recv(BUFSIZE):
+                message = handle_client_messages(data.decode(), index, client_address)
+                client_socket.sendall(message.encode())
+        except Exception:
+            pass  # close the client's connection if they abruptly close
+    # error or EOF - client disconnected
+    client_socket.close()
+    if not (len(message) > 10 and message[:10] == "$UserError"):
+        print(f"Connection from {client_address_users[client_address][0]} closed.")
 
 
-def process_connections(listening_socket):
+def process_connections(listening_socket, index):
     while True:
         client_socket, client_address = listening_socket.accept()
         client_thread = Thread(target=handle_client, 
-                            args=(client_socket, client_address))
+                            args=(client_socket, client_address, index))
         client_thread.start()
 
 
@@ -142,12 +153,13 @@ def process_connections(listening_socket):
 if __name__ == "__main__":
     process_command_line()
     check_valid_file()
-    print(channel_name)
+    print(channel_names)
     print(channel_port)
     print(channel_capacity)
     remaining_ports = len(channel_port)  # will be decrement to check finished port
     channels = [None] * remaining_ports  # store the socket object?
     listening_threads = [None] * remaining_ports  # don't think I need this
+    channel_users = {each_channel: [] for each_channel in channel_names}  # check duplicate users in each channel
     # Thread per listening socket
     for index, port_num in enumerate(channel_port):
         listening_thread = Thread(target=start_server, 
@@ -156,4 +168,4 @@ if __name__ == "__main__":
     while remaining_ports > 0:
         pass
     print("Welcome to chatserver.", flush=True)
-    create_all_ports.set()
+    create_all_ports.set()  # all channels start accepting connections
