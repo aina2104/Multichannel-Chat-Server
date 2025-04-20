ad943196f1d6e4ab142f836a883f2991dada1c42
aina2104 <160872725+aina2104@users.noreply.github.com>
2025-04-20 15:19:24 +1000

    AFK & Shutdown (no queue)
    Basic first connection set up. AFK and shutdown with messages.

--- chatserver.py
+++ chatserver.py
@@ -1,19 +1,21 @@
-from sys import argv, stderr, stdout, exit
+from sys import argv, stderr, stdout, stdin, exit
 from socket import *
-from threading import Event, Lock, Thread, current_thread
+from threading import Event, Thread, current_thread
 from time import sleep
+import os
 
 create_all_ports = Event()
 remaining_ports = -1
-counter = 0
-counter_lock = Lock()
 BUFSIZE = 1024
 afk_time = 100
 config_filename = None
 channels = []
-channel_name = []
+channel_names = []
 channel_port = []
 channel_capacity = []
+channel_queue_length = []  # not yet implemented
+channel_users = {}  # {channel_names: [[user_1, user_2], [user_1_in_queue, user_2_in_queue]]}
+client_address_users = {}  # {client_address: [client_username, channel_name]}
 
 
 def invalid_command_line():
@@ -27,8 +29,8 @@ def invalid_file():
 
 
 def cant_listen(port_num):
-    print(f"Error: unable to listen on port {port_num}.\n")
-    exit(6)
+    print(f"Error: unable to listen on port {port_num}.\n", file=stderr)
+    exit(6)  # maybe have to wait until all unlistened port error msg are printed before exiting
 
 
 def process_command_line():
@@ -56,7 +58,7 @@ def check_channel_name(name):
 
 
 def check_file_format(line):
-    global channel_name
+    global channel_names
     global channel_port
     global channel_capacity
     if len(line) != 4 or line[0] != "channel":
@@ -66,9 +68,9 @@ def check_file_format(line):
         invalid_file()
     port, capacity = int(line[2]), int(line[3])
     if (port < 1024 or port > 65535 or capacity < 1 or capacity > 8 or
-            line[1] in channel_name or port in channel_port):
+            line[1] in channel_names or port in channel_port):
         invalid_file()
-    channel_name.append(line[1])
+    channel_names.append(line[1])
     channel_port.append(port)
     channel_capacity.append(capacity)
 
@@ -86,7 +88,7 @@ def check_valid_file():
 def start_server(port_num, index):
     global channels
     global remaining_ports
-    name = channel_name[index]
+    name = channel_names[index]
     capacity = channel_capacity[index]
 
     listening_socket = socket(AF_INET, SOCK_STREAM)
@@ -102,39 +104,80 @@ def start_server(port_num, index):
           f" of {capacity}.\n", flush=True)
     remaining_ports -= 1  # tell program 1 port has been created
     create_all_ports.wait()  # wait until all ports are created to continue
-    process_connections(listening_socket)
+    process_connections(listening_socket, index)  # generate threads per client
 
 
-def handle_client(client_socket, client_address):
-    global counter
-    host,_ = getnameinfo(client_address, 0)
-    print(f"Connection from {client_address} ({host}) has been established.")
+def duplicate_usernames(username, channel_name):
+    if any(username in queue for queue in channel_users[channel_name]):
+        return True
+    return False
+
+
+# When first accepting client's connection, check for username duplicates, channel capacity,
+# queue capacity and return a message that will be sent to client
+def client_first_connection(client_username, index, client_address):
+    global channel_users
+    global client_address_users
+    channel_name = channel_names[index]
+    # Name duplicates
+    if duplicate_usernames(client_username, channel_name):
+        return f"$UserError: {channel_name}"
+    # If not error, client info will be stored
+    client_address_users[client_address] = [client_username, channel_name]
+    capacity = channel_capacity[index]
+    # Enough capacity to join successfully
+    if len(channel_users[channel_name][0]) < capacity:
+        channel_users[channel_name][0].append(client_username)
+        print(f"[Server Message] {client_username} has joined the channel \"{channel_name}\".\n", file=stdout)
+        return f"$01-JoinSuccess: {channel_name}"
+    # Wait in queue with X users ahead
+    num_users_ahead = len(channel_users[channel_name][1])
+    channel_users[channel_name][1].append(client_username)
+    return f"$01-InQueue: {num_users_ahead}"
+
+
+# Close connection of a client from a channel, if client is in the room,
+# remove them and push any client waiting in the queue.
+# If client in the queue, remove from the queue.
+def remove_from_channel(client_username, channel_name):
+    # Only implement the queue
+    pass
+
+
+def handle_client(client_socket, client_address, index):
     with client_socket:
-        # Send a message to the client
-        message = f"Welcome to the service.\n"
-        client_socket.sendall(message.encode())
+        try:
+            client_socket.settimeout(afk_time)
+            while data := client_socket.recv(BUFSIZE):
+                message = data.decode()
+                if message[:6] == "$User:":
+                    message = client_first_connection(message[7:], index, client_address)
+                    username, channel_name = client_address_users[client_address]
+                client_socket.sendall(message.encode())
+        except client_socket.timeout:
+            # also send this to all clients in the channel
+            print(f"[Server Message] {username} went AFK in channel \"{channel_name}\".\n", file=stdout)
+        except Exception:
+            # close the client's connection if they abruptly close
+            if message[:10] != "$UserError":
+                print(f"Connection from {username} closed.")
+    # error or EOF - client disconnected
+    client_socket.close()  # server handles closing client's socket.
 
-        while data := client_socket.recv(BUFSIZE):
-            # Locking the shared resource (counter)
-            with counter_lock:
-                print(f"Thread {current_thread().name} got the lock.")
-                counter += 1
-                print(f"Counter value updated to: {counter}")
-                # Simulate doing some work
-                sleep(2)
-                data = data.decode().upper() 
-                client_socket.sendall(data.encode())
-        # error or EOF - client disconnected
 
-    print(f"Connection from {client_address} closed.")
-
-
-def process_connections(listening_socket):
+def process_connections(listening_socket, index):
     while True:
         client_socket, client_address = listening_socket.accept()
         client_thread = Thread(target=handle_client, 
-                            args=(client_socket, client_address))
+                            args=(client_socket, client_address, index))
         client_thread.start()
+
+
+# REF: The use of os._exit() is inspired by the code at
+# REF: https://stackoverflow.com/questions/1489669/how-to-exit-the-entire-application-from-a-python-thread
+def server_shutdown():
+    print("[Server Message] Server shuts down.\n")
+    os._exit(0)
 
 
 # REF: The use of Event and their function set(), wait() is inspired by the code at
@@ -142,18 +185,27 @@ def process_connections(listening_socket):
 if __name__ == "__main__":
     process_command_line()
     check_valid_file()
-    print(channel_name)
+    print(channel_names)
     print(channel_port)
     print(channel_capacity)
     remaining_ports = len(channel_port)  # will be decrement to check finished port
     channels = [None] * remaining_ports  # store the socket object?
     listening_threads = [None] * remaining_ports  # don't think I need this
+    channel_users = {each_channel: [[],[]] for each_channel in channel_names}  # check duplicate users in each channel
     # Thread per listening socket
     for index, port_num in enumerate(channel_port):
-        listening_thread = Thread(target=start_server, 
-                args=(port_num, index))
+        listening_thread = Thread(target=start_server, args=(port_num, index))
         listening_thread.start()
     while remaining_ports > 0:
         pass
     print("Welcome to chatserver.", flush=True)
-    create_all_ports.set()
+    create_all_ports.set()  # all channels start accepting connections
+
+    # Main thread starts reading from stdin
+    try:
+        while line := input():
+            if line == "/shutdown":
+                server_shutdown()
+    except Exception:
+        pass
+    print("Server is disconnected.", file=stdout)
